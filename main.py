3c2d29bb357e0895e4900287aa4be76bf6f8775e
MUTINTA <JustinMutinta@hotmail.com>
2021-08-28 20:57:11 -0500

    Average height of students using loops

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-fruits = ["Apple", "Peach", "Pear", "Orange", "Tomato", "Avocado"]
+student_heights = input("Input a list of student heights ").split()
 
-for fruit in fruits:
-    print(fruit)
-    print(fruit + " Pie")
-print(fruits)
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+
+print(student_heights)
+
+total_height = 0
+
+for y in range(0, len(student_heights)):
+    total_height += student_heights[y]
+
+print(total_height)
+
+average_height = total_height / len(student_heights)
+
+print(average_height)
